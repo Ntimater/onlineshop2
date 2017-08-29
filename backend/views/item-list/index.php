fac55182628d9317a0336ae9f3083e63b9e8b4df
unknown <ntimater2000@gmail.com>
2017-08-29 18:47:38 +0900

    Second commit

--- backend/views/item-list/index.php
+++ backend/views/item-list/index.php
@@ -3,25 +3,65 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 ?>
-
-<?php foreach ($shops as $abc): ?>
-
-    <p><?= $abc->NameShop ?> </p>
-    <?php foreach ($abc->getItemes() as $itemes):
-
-        $i=$itemes->IdItem;
+<div class="container" >
+    <h3><u> <center><div class="intro-lead-in" >AVAILABLE ITEMES LIST </div> <center></u> </h3>
 
 
-        ?>
+    <?php foreach ($shops as $abc): ?>
 
-        <ul>
+            <p><?= $abc->NameShop ?> </p>
 
-             <li><?= $itemes->Description ?> , <?= $itemes->Quantity ?>,<?= $itemes->Price?> , <?= $itemes->Categories ?>,<?= $itemes->DedicatePers?></li>
-            <td width="112"><img src="img/IT009.jpg" width="130" height="100" /></td>
+                <?php foreach ($abc->getItemes() as $index => $itemes):?>
+                <?php if ($index % 3 == 0): ?> <div class="row"> <?php endif; ?>
+                <div class="col-md-4">
+                <div class="thumbnail">
+                     <ul>
+                        <li>
 
-        </ul>
-    <?php endforeach;?>
+                            <?= $itemes->Description ?> ,
+                            <?= $itemes->Quantity ?>,
+                            <?= $itemes->Price?> ,
+                            <?= $itemes->Categories ?>,
+                            <?= $itemes->DedicatePers?>
+                            <hr><img src="http://www.nyc.gov.rw/uploads/RTEmagicC_Tshirt.jpg.jpg" alt="google.com">
+                            <div class="radio-group">
+                                <form action="">
+                                    <input type="radio" name="gender" value="1"> Buy
+                                    <input type="radio" name="gender" value="2"> Booking
+                                    <input type="submit" value="Approve">
 
-    <hr>
-<?php endforeach; ?>
+                                </form>
+                            </div>
+
+
+                        </li>
+
+
+                    </ul>
+                </div>
+                </div>
+        <?php if ($index % 3 == 0): ?> </div> <?php endif; ?>
+
+
+
+            <?php endforeach;?>
+        </div>
+
+        <hr>
+
+    <?php endforeach; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
