bc942b687aa6718a70156875bb34c40bc5bd6768
unknown <ntimater2000@gmail.com>
2017-08-31 10:29:55 +0900

    Fourth commit

--- backend/views/item-list/index.php
+++ backend/views/item-list/index.php
@@ -1,72 +1,51 @@
 <?php
-
 use yii\helpers\Html;
 use yii\grid\GridView;
 ?>
 <div class="container" >
-    <h3><u> <center><div class="intro-lead-in" >AVAILABLE ITEMES LIST </div> <center></u> </h3>
+    <h3><b><center><div class="intro-lead-in" >MARKET</div> <center></b></h3>
 
 
     <?php foreach ($shops as $abc): ?>
 
-            <p><?= $abc->NameShop ?> </p>
+        <h3><p><?= $abc->NameShop?></p></center></h3>
 
-                <?php foreach ($abc->getItemes() as $index => $itemes):?>
-                <?php if ($index % 3 == 0): ?> <div class="row"> <?php endif; ?>
-                <div class="col-md-4">
-                <div class="thumbnail">
-                     <ul>
-                        <li>
+        <?php foreach ($abc->getItemes() as $index => $itemes):?>
+        <?php if ($index % 3 == 0): ?> <div class="row"> <?php endif; ?>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                 <ul>
+                    <li>
 
-                            <?= $itemes->Description ?> ,
-                            <?= $itemes->Quantity ?>,
-                            <?= $itemes->Price?> ,
-                            <?= $itemes->Categories ?>,
-                            <?= $itemes->DedicatePers?>
-                            <hr><img src="http://www.nyc.gov.rw/uploads/RTEmagicC_Tshirt.jpg.jpg" alt="google.com">
-                            <div class="radio-group">
-                                <form action="">
-                                    <input type="radio" name="gender" value="1"> Buy
-                                    <input type="radio" name="gender" value="2"> Booking
-                                    <input type="submit" value="Approve">
+                        <?= $itemes->Description ?> ,
+                        <?= $itemes->Quantity ?>,
+                        <font color="red"><?= $itemes->Price ?></font>,
+                        <?= $itemes->Categories ?>,
+                        <?= $itemes->DedicatePers?>
+                        <hr><img src="http://www.nyc.gov.rw/uploads/RTEmagicC_Tshirt.jpg.jpg" alt="google.com">
+                        <div class="radio-group">
+                            <form action="">
+                                <input type="radio" name="gender" value="1"> Buy
+                                <input type="radio" name="gender" value="2"> Booking
+                                <input type="submit" value="Approve">
 
-                                </form>
-                            </div>
-                        </li>
-
-
-                    </ul>
-                </div>
-
-                </div>
+                            </form>
+                        </div>
+                    </li>
 
 
-
-
-
-        <?php if (($index + 1) % 3 == 0): ?> </div><?php endif; ?>
-
-    <?php endforeach;?>
-
-
+                </ul>
+            </div>
 
         </div>
-        <hr>
 
+
+
+
+
+        <?php if (($index + 1) % 3 == 0 || count($abc->getItemes())==($index+1)): ?> </div><?php endif; ?>
+
+    <?php endforeach;?>
+        <hr>
     <?php endforeach; ?>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
